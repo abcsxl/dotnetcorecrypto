5e67dd0cb96e36f689075c5ec9bbf732534563fd
SIM <abcsxl@msn.com>
2017-07-03 11:05:19 +0800

    TripleDES

--- bouncycastledes/Program.cs
+++ bouncycastledes/Program.cs
@@ -1,9 +1,4 @@
 ﻿using dotnetcorecrypto;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Engines;
-using Org.BouncyCastle.Crypto.Modes;
-using Org.BouncyCastle.Crypto.Paddings;
-using Org.BouncyCastle.Crypto.Parameters;
 using System;
 
 namespace bouncycastledes
@@ -12,19 +7,18 @@ namespace bouncycastledes
     {
         static void Main(string[] args)
         {
-            IBlockCipher engine = new DesEngine();
 
             byte[] key = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF".HexToByteArray();
-            byte[] data = "1122334455667788".HexToByteArray();
+            byte[] data = "112233445566778899".HexToByteArray();
             byte[] iv = "0000000000000000".HexToByteArray();
 
-            BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine), new Pkcs7Padding());
-            cipher.Init(true, new ParametersWithIV(new DesParameters(key), iv));
-            byte[] rv = new byte[cipher.GetOutputSize(data.Length)];
-            int tam = cipher.ProcessBytes(data, 0, data.Length, rv, 0);
-            cipher.DoFinal(rv, tam);
+            TripleDES tripleDes = new TripleDES();
 
-            Console.WriteLine(rv.ByteArrayToHex());
+            byte[] res = tripleDes.Encrypt(key, iv, data);
+            Console.WriteLine(res.ByteArrayToHex());
+
+            res = tripleDes.Decrypt(key, iv, res);
+            Console.WriteLine(res.ByteArrayToHex());
 
             Console.ReadKey(true);
         }
---
+++ bouncycastledes/TripleDES.cs
@@ -0,0 +1,35 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace bouncycastledes
+{
+    class TripleDES
+    {
+        IBlockCipher engine = new DesEngine();
+
+        public byte[] Encrypt(byte[] key, byte[] iv, byte[] data)
+        {
+            BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine), new ISO7816d4Padding());
+            cipher.Init(true, new ParametersWithIV(new DesParameters(key), iv));
+            byte[] rv = new byte[cipher.GetOutputSize(data.Length)];
+            int tam = cipher.ProcessBytes(data, 0, data.Length, rv, 0);
+            cipher.DoFinal(rv, tam);
+
+            return rv;
+        }
+
+        public byte[] Decrypt(byte[] key, byte[] iv, byte[] data)
+        {
+            BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine), new ISO7816d4Padding());
+            cipher.Init(false, new ParametersWithIV(new DesParameters(key), iv));
+            byte[] rv = new byte[cipher.GetOutputSize(data.Length)];
+            int tam = cipher.ProcessBytes(data, 0, data.Length, rv, 0);
+            cipher.DoFinal(rv, tam);
+
+            return rv;
+        }
+    }
+}
